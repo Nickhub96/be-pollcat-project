a227dfb2e2a5d22daf0ec380db01e4a2efaa4068
Iffa Hossain <iffa26@users.noreply.github.com>
2020-02-18 14:50:52 +0000

    connected to AWS MySQL db and posted dummy data

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-//
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+app.use(express.json());
+
+const connection = mysql.createConnection({
+  host: "pollcat-mysqldb-instance-1.cw9az4fgwt3t.eu-west-2.rds.amazonaws.com",
+  user: "pollcatadmin",
+  password: "Team.Pollcat1"
+});
+
+// post request to create a user
+app.post("/users", (req, res) => {
+  console.log(req.query);
+  console.log(req.params);
+  if (req.query.username && req.query.email && req.query.age) {
+    console.log("Request received");
+
+    connection.connect(function(err) {
+      connection.query(
+        `INSERT INTO main.users (username, email, age) VALUES ('${req.query.username}', '${req.query.email}', '${req.query.age}')`,
+        function(err, result, fields) {
+          if (err) res.send(err);
+          if (result)
+            res.send({
+              username: req.query.username,
+              email: req.query.email,
+              age: req.query.age
+            });
+          if (fields) console.log(fields);
+        }
+      );
+    });
+  } else {
+    console.log("Missing a parameter");
+  }
+});
+
+// get request for user data
+app.get("/users", (req, res) => {
+  connection.connect(function(err) {
+    connection.query(`SELECT * FROM main.users`, function(err, result, fields) {
+      if (err) res.send(err);
+      if (result) res.send(result);
+    });
+  });
+});
+
+const { PORT = 9090 } = process.env;
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
